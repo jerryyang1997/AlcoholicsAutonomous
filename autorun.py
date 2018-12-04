29cf4c1117e6e194cfa7d2a8c7633812a91ec4bf
jerryyang1997 <yiy116@ucsd.edu>
2018-12-04 09:44:27 -0800

    added files

---
+++ autorun.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+PUMP1 = 6
+PUMP2 = 27
+PUMP3 = 17
+PUMP4 = 5
+
+GPIO.setup(PUMP1,GPIO.OUT)
+GPIO.setup(PUMP2,GPIO.OUT)
+GPIO.setup(PUMP3,GPIO.OUT)
+GPIO.setup(PUMP4,GPIO.OUT)
+
+GPIO.output(PUMP1,GPIO.LOW)
+GPIO.output(PUMP2,GPIO.LOW)
+GPIO.output(PUMP3,GPIO.LOW)
+GPIO.output(PUMP4,GPIO.LOW)
