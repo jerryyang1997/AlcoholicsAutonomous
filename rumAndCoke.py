29500fdce1e58dea93db5ab93678fc8611022d0f
jerryyang1997 <yiy116@ucsd.edu>
2018-12-04 10:23:10 -0800

    add comment

--- rumAndCoke.py
+++ rumAndCoke.py
@@ -1,6 +1,8 @@
+#import modules
 import RPi.GPIO as GPIO
 import time
 
+#set output pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 PUMP1 = 6
@@ -13,16 +15,25 @@ GPIO.setup(PUMP2, GPIO.OUT)
 GPIO.setup(PUMP3, GPIO.OUT)
 GPIO.setup(PUMP4, GPIO.OUT)
 
+#turn on all pumps for 10 seconds
 GPIO.output(PUMP1,GPIO.HIGH)
 GPIO.output(PUMP2,GPIO.HIGH)
 GPIO.output(PUMP3,GPIO.HIGH)
 GPIO.output(PUMP4,GPIO.HIGH)
 time.sleep(10)
+
+#turn pump1 off
 GPIO.output(PUMP1,GPIO.LOW)
 time.sleep(5)
+
+#turn pump2 off
 GPIO.output(PUMP2,GPIO.LOW)
 time.sleep(5)
+
+#turn pump3 off
 GPIO.output(PUMP3,GPIO.LOW)
 time.sleep(5)
+
+#turn pump4 off
 GPIO.output(PUMP4,GPIO.LOW)
 
--- screwDriver.py
+++ screwDriver.py
@@ -1,6 +1,8 @@
+#import modules
 import RPi.GPIO as GPIO
 import time
 
+#declare output pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 PUMP1 = 6
@@ -13,30 +15,36 @@ GPIO.setup(PUMP2, GPIO.OUT)
 GPIO.setup(PUMP3, GPIO.OUT)
 GPIO.setup(PUMP4, GPIO.OUT)
 
+#turn pumps 1 and 4 on for 5 seconds and turn them off
 GPIO.output(PUMP1,GPIO.HIGH)
 GPIO.output(PUMP4,GPIO.HIGH)
 time.sleep(5)
 GPIO.output(PUMP1,GPIO.LOW)
 GPIO.output(PUMP4,GPIO.LOW)
 
+#turn pumps 2 and 3 on for 5 seconds and turn them off
 GPIO.output(PUMP2,GPIO.HIGH)
 GPIO.output(PUMP3,GPIO.HIGH)
 time.sleep(5)
 GPIO.output(PUMP3,GPIO.LOW)
 GPIO.output(PUMP2,GPIO.LOW)
 
+#turn pump 1 on for 2 seconds and turn it off
 GPIO.output(PUMP1,GPIO.HIGH)
 time.sleep(2)
 GPIO.output(PUMP1,GPIO.LOW)
 
+#turn pump 2 on for 2 seconds and turn it off
 GPIO.output(PUMP2,GPIO.HIGH)
 time.sleep(2)
 GPIO.output(PUMP2,GPIO.LOW)
 
+#turn pump 3 on for 2 seconds and turn it off
 GPIO.output(PUMP3,GPIO.HIGH)
 time.sleep(2)
 GPIO.output(PUMP3,GPIO.LOW)
 
+#turn pump4 on for 2 seconds and turn it off
 GPIO.output(PUMP4,GPIO.HIGH)
 time.sleep(2)
 GPIO.output(PUMP4,GPIO.LOW)
